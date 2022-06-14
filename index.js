ab187552e7877a1288cf3c26e8c1c1e4dcc28140
Bruna <brunac.cardososilva@gmail.com>
2022-06-14 12:25:19 -0300

    Requisito 1 e 2
    Em desenvolvimento
    
    Concluído.

--- index.js
+++ index.js
@@ -15,3 +15,22 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+const fs = require('fs');
+
+fs.readFileSync('./talker.json');
+
+const talker = require('./talker');
+
+app.get('/talker', (_req, res) => {
+  if (!talker) return res.status(200).json([]);
+
+  res.status(200).json(talker);
+});
+
+app.get('/talker/:id', (req, res) => {
+const { id } = req.params;
+const talkerIndex = talker.find((t) => t.id === Number(id));
+if (!talkerIndex) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+res.status(200).json(talkerIndex);
+});
